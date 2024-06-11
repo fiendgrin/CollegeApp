9b962c996ef85ede5dc21dbe5062232316a74f99
Yunis <yunisah@code.edu.az>
2024-06-11 23:31:56 +0400

    Auto mapper with different property names, ignore, transform

--- CollegeApp/Configurations/AutoMapperConfig.cs
+++ CollegeApp/Configurations/AutoMapperConfig.cs
@@ -8,7 +8,14 @@ namespace CollegeApp.Configurations
     {
         public AutoMapperConfig()
         {
+            //configuration for different name properties
+            //CreateMap<Student, StudentDTO>().ReverseMap().ForMember(x=>x.StudentName, opt=>opt.MapFrom(x=>x.Name));
+            //configuration for ignoring some properties
+            //CreateMap<Student, StudentDTO>().ReverseMap().ForMember(x => x.Name,opt=>opt.Ignore());
+            //configuration for transforming some properties
+            //CreateMap<Student, StudentDTO>().ReverseMap().AddTransform<string>(x => string.IsNullOrEmpty(x)?"No Address":x);
             CreateMap<Student, StudentDTO>().ReverseMap();
+
         }
     }
 }
